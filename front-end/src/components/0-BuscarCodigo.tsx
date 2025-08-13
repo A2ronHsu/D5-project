9a0f0edcf0d9b9c88d1b4ba2deadc8839edcefce
A2ron <aaronbee1990@gmail.com>
2025-08-13 08:55:42 -0300

    added a Link to DannyHome

--- front-end/src/components/0-BuscarCodigo.tsx
+++ front-end/src/components/0-BuscarCodigo.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './1-Nav';
 import Form from './2-Form';
+import { Link } from 'react-router-dom';
 
 const BuscarCodigo: React.FC = ()=> {
  return (
   <>
     <Nav></Nav>
     <Form></Form>
+    <Link to="/dannyhome/transfer" className='link_button'>DannyHome</Link>
   </>
  )
 }
