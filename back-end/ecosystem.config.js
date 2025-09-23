73d16820445741c4639d2db9c8f1fa08ff505a2e
A2ron <aaronbee1990@gmail.com>
2025-09-23 13:50:49 -0300

    fixed a pm2 ecosystem.config script path

---
+++ back-end/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+    name   : "server",
+    script : "dist/server.js",
+    env: {
+      NODE_ENV: "development"
+    },
+    env_production: {
+      NODE_ENV: "production"
+    }
+  }],
+  deploy: {
+    production: {
+      env: {
+        // Load environment variables from a file
+        // You can replace this with your actual .env file path
+        "source_env": ".env" 
+      }
+    }
+  }
+};
